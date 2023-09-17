2ecf1c16a89708d8c7ec205b2055de34ac62bc73
GeorgesLevyEtoundi <etoundioyono.georgeslevy@gmail.com>
2023-09-17 21:23:37 +0100

    modified the compnents and rewrite Navbar et About component

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const pageLinks = [
+	{ id: 1, href: '#home', name: 'Home' },
+	{ id: 2, href: '#about', name: 'About' },
+	{ id: 3, href: '#services', name: 'Services' },
+	{ id: 4, href: '#tours', name: 'Tours' },
+]
+
+export const navIcons = [
+	{ id: 1, href: 'https://www.twitter.com', icon: 'fab fa-facebook' },
+	{ id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
+	{ id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
+]
